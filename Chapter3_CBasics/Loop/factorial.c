8972cd56e551c5b6d077116e58cc589d05d7302b
Sirjan Shrestha <sirjanshrestha13@gmail.com>
2022-02-25 19:31:08 +0545

    all

---
+++ Chapter3_CBasics/Loop/factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main()
+{
+     int a,fact=1; 
+
+ printf("Enter a number:\a");
+
+  scanf("%d",&a);
+
+    for(a =1;a<=a;a++)
+    {
+
+      fact=fact*a;  
+
+  }    
+  
+  printf("Factorial of %d is: %d",fact);    
+
+return 0;  
+
+}   
